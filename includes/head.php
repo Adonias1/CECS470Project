c2789859357d957da5de7ed3cbbb04380d004560
Brett Lockhart <blockhart615@gmail.com>
2016-04-21 11:47:48 -0700

    Initial Commit Include Files
    PHP "includes" so if we need to make any changes to head, header, or
    footer, we only need to make the change in one place.

---
+++ includes/head.php
@@ -0,0 +1,5 @@
+<title>Rachel Lockhart</title>
+<link rel="stylesheet" type="text/css" href="styles/reset.css">
+<link rel="stylesheet" type="text/css" href="styles/styles.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header>
+	<h1 class="rachel-title"><a href="index.php">Rachel Lockhart</a></h1>
+	<nav>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="resume.php">Resume</a></li>
+		<li><a href="projects.php">Projects</a></li>
+		<li><a href="headshots.php">Headshots</a></li>
+		<li><a href="reel.php">Reel</a></li>
+		<li><a href="about.php">About</a></li>
+		<li><a href="contact.php">Contact</a></li>
+	</nav>
+</header>
