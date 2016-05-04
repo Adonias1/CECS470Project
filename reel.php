9bdb78805580e85b87f394b87acbdd4cf334310b
Brett Lockhart <blockhart615@gmail.com>
2016-05-04 16:14:01 -0700

    Styles update
    More playing with styles.

--- reel.php
+++ reel.php
@@ -11,15 +11,19 @@
 
 		<!--TODO FILL IN BODY -->
 		<!-- Love and Miss Lily -->
+		<h2 class="reel-title">Love and Miss Lily</h2>
 		<iframe class="reel" width="560" height="315" src="https://www.youtube.com/embed/a-vJ-RwiukU" frameborder="0" allowfullscreen></iframe>
 
 		<!-- Kra Magaga -->
+		<h2 class="reel-title">Kra Magaga</h2>
 		<iframe class="reel" width="560" height="315" src="https://www.youtube.com/embed/U-lEYLmSsLQ" frameborder="0" allowfullscreen></iframe>
 
 		<!-- Kra Magaga 2 -->
+		<h2 class="reel-title">Kra Magaga 2</h2>
 		<iframe class="reel" width="560" height="315" src="https://www.youtube.com/embed/MpY9H-EKkyc" frameborder="0" allowfullscreen></iframe>
 
 		<!-- Schindler -->
+		<h2 class="reel-title">Schindler</h2>
 		<iframe class="reel" width="560" height="315" src="https://www.youtube.com/embed/enkpVpLt-cQ" frameborder="0" allowfullscreen></iframe>
 
 		<?php include "includes/footer.php"; ?>
