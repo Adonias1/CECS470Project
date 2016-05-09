12be7aa6b930f336c555bb471ad60c47fd8ff3b2
Brett Lockhart <blockhart615@gmail.com>
2016-05-09 13:42:53 -0700

    Add rest of resume. Add search function (still needs to have reset functionality). Adjust styles.

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <header>
 	<div id="logo">
 		<h1 class="rachel-title"><a href="index.php">Rachel Lockhart</a></h1>
+		<h2 class="rachel-title"><a href="index.php">Actress</a></h2>
 	</div>
 	<nav>
 		<li><a href="index.php">Home</a></li>
