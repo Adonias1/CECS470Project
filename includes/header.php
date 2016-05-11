0d15a5e470eb9aac27efe232a46cda0bf1f783a0
Brett Lockhart <blockhart615@gmail.com>
2016-05-10 17:54:03 -0700

    Add bio to home page.

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,11 @@
 	</div>
 	<nav>
 		<li><a href="index.php">Home</a></li>
-		<li><a href="projects.php">News/Projects</a></li>
+		<li><a href="projects.php">News</a></li>
 		<li><a href="headshots.php">Headshots</a></li>
 		<li><a href="reel.php">Reel</a></li>
 		<li><a href="resume.php">Resume</a></li>
-		<li><a href="about.php">About</a></li>
+		<!-- <li><a href="about.php">About</a></li> -->
 		<li><a href="contact.php">Contact</a></li>
 	</nav>
 </header>
