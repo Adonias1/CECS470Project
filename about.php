158271b7da6b4a1a8d7aab632b11079ab7ae85dd
Brett Lockhart <blockhart615@gmail.com>
2016-05-10 17:26:26 -0700

    Fill in about page. More styles. Center nav elements

--- about.php
+++ about.php
@@ -8,9 +8,16 @@
 <body>
 	<?php include "includes/header.php"; ?>
 	<div class="main-content">
-
-		<!--TODO FILL IN BODY -->
-
+		<img id="about-image" src="images/theater.jpg" alt="Image of Rachel Lockhart acting in a play" />
+		<p id="bio">
+			Rachel Lockhart's love of acting started at a young age. As a California native, she attended the Orange County School of the Arts from grades 7-12, studying acting. From there, she received a scholarship to study acting at Brigham Young University.
+		</p>		
+		<p>
+			To supplement her education, she trained at the Yale School of Drama's Summer Shakespeare Intensive. After graduating, she moved to Europe where she studied French and performed. She has a deep love and respect for theater, but found that her passion was in telling stories through film.
+		</p>
+		<p>
+			She currently resides in Los Angeles, California, where she enjoys pursuing her passion and collaborating	with other talented artists. You can see her work on recent television shows like Criminal Minds and	in various theater performances around Los Angeles.
+		</p>
 		<?php include "includes/footer.php"; ?>
 	</div>
 </body>
--- includes/header.php
+++ includes/header.php
@@ -5,10 +5,10 @@
 	</div>
 	<nav>
 		<li><a href="index.php">Home</a></li>
-		<li><a href="resume.php">Resume</a></li>
 		<li><a href="projects.php">Projects</a></li>
 		<li><a href="headshots.php">Headshots</a></li>
 		<li><a href="reel.php">Reel</a></li>
+		<li><a href="resume.php">Resume</a></li>
 		<li><a href="about.php">About</a></li>
 		<li><a href="contact.php">Contact</a></li>
 	</nav>
