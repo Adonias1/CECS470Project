38a6faf7c26d47f16bc9e3a1bf426366aa52e5ab
Gary4279 <nunezgary4279@yahoo.com>
2016-05-11 00:56:37 -0700

    asd
    asd

--- contact.php
+++ contact.php
@@ -6,37 +6,31 @@
 	?>
 <head>
 	<?php include "includes/head.php" ?>
+	<script src="contact.js"></script>
 </head>
 <?php
-//echo "before connect<br>";
-$connection = mysqli_connect("cecs-db01.coe.csulb.edu","cecs323o32","iehohx","cecs470g6");
-// mysqli_connect_error returns string description of the last
-// connect error
-//echo "after connect<br>";
-//check for connection error
-$error = mysqli_connect_error();
-//if there is a connection error...
-if ($error != null) {
-  $output = "<p>Unable to connect to database<p>" . $error;
-  // Outputs a message and terminates the current script
-  exit($output);
-  }
-  //echo "connected<br>";
-  //create the sql statement
-  $sql = "INSERT INTO Prospects";
-  //echo "before query<br>";
-  //exectue the query
-  $result = mysqli_query($connection, $sql);
-  //echo "after query<br>";
-  //find out how many rows are in the result set
-  $numrows=mysqli_num_rows($result);
-  //echo "The number of rows is: ".$numrows."<br>";
-  //loop through the result set
-  if ($result=mysqli_query($connection,$sql))
-    {
+	$connection = mysqli_connect("cecs-db01.coe.csulb.edu","cecs323o32","iehohx","cecs470g6");
+	$error = mysqli_connect_error();
+	//if there is a connection error...
+	if ($error != null) {
+		$output = "<p>Unable to connect to database<p>" . $error;
+		// Outputs a message and terminates the current script
+		exit($output);
+	}
+  	//echo "connected<br>";
+  	//create the sql statement
+  	$sql = "INSERT INTO Prospects";
+  	//echo "before query<br>";
+  	//exectue the query
+  	$result = mysqli_query($connection, $sql);
+  	//echo "after query<br>";
+  	//find out how many rows are in the result set
+  	$numrows=mysqli_num_rows($result);
+  	//echo "The number of rows is: ".$numrows."<br>";
+  	//loop through the result set
+  	if ($result=mysqli_query($connection,$sql)){
     // Fetch one and one row
-    while ($row=mysqli_fetch_assoc($result))
-	{
+    while ($row=mysqli_fetch_assoc($result)){
                 echo $row["FirstName"]." ".$row["LastName"];
 				echo $row["Email"];
 				echo $row["Message"];
@@ -59,7 +53,7 @@ if ($error != null) {
 		</p><br>
 
 
-		<form id = "mainForm" action = "contact.php" method = "POST">
+		<form id = "mainForm" action = "" method = "POST">
 		<?php
 		if($_SERVER["REQUEST_METHOD"]=="POST"){
 			if(!empty($_POST["firstname"])){
@@ -111,43 +105,44 @@ if ($error != null) {
 				$sql = "INSERT INTO Prospects(FirstName, LastName, Email, Message) VALUES('".$firstname."', 
 				'".$lastname."', '".$email."', '".$message."')";
 				//echo "before query<br>";
-			//exectue the query
-			$result = mysqli_query($connection, $sql);
-			//echo "after query<br>";
-			//find out how many rows are in the result set
-			$numrows=mysqli_num_rows($result);
-			//echo "The number of rows is: ".$numrows."<br>";
-			//loop through the result set
-			if ($result=mysqli_query($connection,$sql))
-				{
-			// Fetch one and one row
-				while ($row=mysqli_fetch_assoc($result))
-				{
-					echo $row["FirstName"]." ".$row["LastName"];
-					echo $row["Email"];
-					echo $row["Message"];
+				//exectue the query
+				$result = mysqli_query($connection, $sql);
+				//echo "after query<br>";
+				//find out how many rows are in the result set
+				$numrows=mysqli_num_rows($result);
+				//echo "The number of rows is: ".$numrows."<br>";
+				//loop through the result set
+				if ($result=mysqli_query($connection,$sql)){
+					// Fetch one and one row
+					while ($row=mysqli_fetch_assoc($result)){
+						echo $row["FirstName"]." ".$row["LastName"];
+						echo $row["Email"];
+						echo $row["Message"];
+					}
+
+    					// Free result set
+					mysqli_free_result($result);
+
 				}
-
-    // Free result set
-			mysqli_free_result($result);
-
+				else { 
+					echo "no result<br>";
+				}
 			}
-			else { echo "no result<br>";}
    		?>
 			<section>
 				<fieldset>
 					<!-- <h3>Enter Personal Info: </h3> -->
 					<p><span class = "error"><?php echo $requiredFields;?></span></p>
 					<label>First Name:</label><br/>
-					<input type="text" name="firstname" size="30">
+					<input id = fname type="text" name="firstname" size="30" required>
 					<span class = "error"><?php echo $fNameErr;?></span><br>
 					<br>
 					<label>Last Name:</label><br/>
-					<input type="text" name="lastname" size="30">
+					<input id = lname type="text" name="lastname" size="30" required>
 					<span class = "error"><?php echo $lNameErr;?></span><br>
 					<br>
 					<label>Email:</label><br>
-					<input type="email" name="email" size="30">
+					<input id = email type="email" name="email" size="30" required>
 					<span class = "error"><?php echo $emailErr;?></span><br>
 					<br>
 					<label><p>Message:</p></label>
